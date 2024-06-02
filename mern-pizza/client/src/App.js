da481ad73f922dc8af8170a049e9ddfa0bdd9968
janhavi22 <janhavijchitade@gmail.com>
2024-06-02 22:16:11 +0530

    new

--- mern-pizza/client/src/App.js
+++ mern-pizza/client/src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
-import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import Navbar from './componants/navbar'
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+import Navbar from './componants/navbar';
+
 function App() {
   return (
     <div className="App">
+ 
      <Navbar/>
+
     </div>
   );
 }
